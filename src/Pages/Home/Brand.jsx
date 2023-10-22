b6f325a4f041743f6a99d3d885811daa190402a5
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-23 01:20:02 +0530

    Added brand card by fetching data from databse and make theme responsive

--- src/Pages/Home/Brand.jsx
+++ src/Pages/Home/Brand.jsx
@@ -4,17 +4,17 @@ import { AuthContext } from '../../AuthProvider/AuthProvider';
 const Brand = ({brand}) => {
     // destructure 
     const {brand_name, image_url, short_description} = brand;
-    const {darkMode, setDarkMode} = useContext(AuthContext);
+    const {darkMode} = useContext(AuthContext);
 
     return (
-        <div>
-            <div className="card card-compact bg-base-100 shadow-xl">
+        <div style={{backgroundColor: darkMode==="true" ? '#1D232A':'#F0EFF5', color: darkMode==="true" ? 'white': '#1D232A'}} className='rounded-lg mb-10'>
+            <div className="card card-compact shadow-xl pt-4 rounded-lg">
             <figure className='w-[350px] h-[250px] rounded-xl bg-white mx-auto my-auto object-cover object-center p-4'><img src={image_url} alt={brand_name} /></figure>
-            <div style={{backgroundColor: darkMode==="true" ? '#1D232A':'white', color: darkMode==="true" ? 'white': '#1D232A'}}   className="card-body p-4 h-[200px]">
-                <h2 className="card-title">{brand_name}</h2>
-                <p>{short_description}</p>
-                <div className="card-actions">
-                <button className="btn btn-primary">Visit Our Brand</button>
+            <div className=" p-4 h-[200px]">
+                <h2 className="card-title text-red-500  font-bold text-2xl pl-2">{brand_name}</h2>
+                <p className='pl-2 mt-2 h-[100px]'>{short_description}</p>
+                <div className='flex justify-center'>
+                <button className="btn mx-auto bg-[#123385] hover:bg-[#1f4bbb] text-white py-3 px-3 rounded-2xl my-5 font-bold">Visit Our Brand</button>
                 </div>
             </div>
             </div>
