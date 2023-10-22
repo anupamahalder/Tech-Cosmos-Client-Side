965c82940d6651526b1695f30920c43aeb3bf1bd
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-22 18:34:02 +0530

    Created responsive navbar for mobile devices

--- src/Layout/Header/Navbar.jsx
+++ src/Layout/Header/Navbar.jsx
@@ -1,20 +1,29 @@
 import { NavLink } from 'react-router-dom';
 import {BsFillBrightnessHighFill, BsFillMoonFill} from 'react-icons/bs';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
+import {AiOutlineAlignRight, AiOutlineDown} from 'react-icons/ai';
 const Navbar = () => {
     // destructure 
     const {darkMode, setDarkMode} = useContext(AuthContext);
+    // declare a state to hold value of mobile navbar 
+    const [isOpen, setIsOpen] = useState(false);
     // console.log(darkMode);
     return (
         <div style={{backgroundColor: darkMode==="true" ? '#0c2461':'white', color: darkMode==="true" ? 'white': '#0c2461'}}  
-        className="flex justify-between px-20 font-semibold text-xl py-6 shadow-lg">
+        className="flex justify-between px-10 md:px-20 font-semibold text-xl py-6 shadow-lg">
             <div>
                 {/* logo  */}
                 <h1 className='text-3xl font-bold'>Tech Cosmos</h1>
             </div>
-            <div className='flex gap-10'>
-                
+            {/* style only for mobile devices  */}
+            <div className='md:hidden text-2xl' onClick={()=>setIsOpen(!isOpen)}>
+                {
+                    isOpen==true ? <AiOutlineDown></AiOutlineDown>:
+                    <AiOutlineAlignRight></AiOutlineAlignRight>
+                }
+            </div>
+            <div className={`md:flex md:gap-10 absolute md:static ${isOpen ? "rounded-xl mt-16 p-4 z-10 flex flex-col right-6":"-top-80 block"} }`} style={{backgroundColor: darkMode==="true" ? '#0c2461':'white', color: darkMode==="true" ? 'white': '#0c2461'}}>
                 {/*------------ Navlink------------  */}
                 <NavLink to='/'className={({ isActive, isPending }) =>
                 isPending ? "pending" : isActive ? "text-red-500" : ""}>Home</NavLink>
