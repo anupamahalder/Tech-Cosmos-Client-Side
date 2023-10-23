6c7c2fd5abe0c6bb70be099c981b21e9c7dd4632
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-23 22:25:14 +0530

    Adjusting the elements in products card

--- src/Pages/Brand/BrandDisplay.jsx
+++ src/Pages/Brand/BrandDisplay.jsx
@@ -11,10 +11,13 @@ const BrandDisplay = ({brandInfo}) => {
         <div style={{backgroundColor: darkMode==="true" ? '#1D232A':'#F0EFF5', color: darkMode==="true" ? 'white': '#1D232A'}} className='rounded-lg mb-10'>
             <div className="card card-compact shadow-xl pt-4 rounded-lg">
             <figure style={{backgroundColor: darkMode==="true" ? '#1D232A':'#F0EFF5'}}
-             className='w-[350px] h-[250px] rounded-xl mx-auto my-auto object-cover object-center p-4'><img className='rounded-lg' src={image} alt={type} /></figure>
-            <div className=" p-4 h-[200px]">
-                <h2 className="card-title text-red-500  font-bold text-2xl pl-2">{type}</h2>
-                <p className='pl-2 mt-2 h-[100px]'>{category}</p>
+             className='h-[250px] mx-auto my-auto object-cover object-center'><img className='' src={image} alt={type} /></figure>
+            <div className="p-4">
+                <h2 className="card-title font-bold text-3xl pl-2">{name}</h2>
+                <p className='pl-2 my-2'>{category}</p>
+                <p className='pl-2'>Type: <span>{type}</span></p>
+                <p className='pl-2'>Price: <span>{price}</span></p>
+                {/* ratings  */}
                 <div className='flex justify-center gap-5'>
                 <Link>
                 <button className="btn mx-auto bg-[#123385] hover:bg-[#1f4bbb] text-white py-3 px-3 rounded-2xl my-5 font-bold">Details</button></Link>
